a6179d8c580adc6ca22630542dd61b44c0d5016d
kuzma.shapran <Kuzma.Shapran@gmail.com>
2011-09-18 09:24:20 +0000

    * License info fixed

--- colour_space_viewer.pro
+++ colour_space_viewer.pro
@@ -1,8 +1,33 @@
-QT += core gui
+##############
+##
+## Project:      Colour-space-viewer
+## File info:    $Id$
+## Author:       Copyright (C) 2011 Kuzma Shapran <Kuzma.Shapran@gmail.com>
+## License:      GPLv3
+##
+##  This file is part of Colour-space-viewer.
+##
+##  Colour-space-viewer is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  Colour-space-viewer is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with Colour-space-viewer.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############
+
 
 TARGET = colour_space_viewer
+
 TEMPLATE = app
 
+QT += core gui
 
 SOURCES += \
 	main.cpp\
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,24 +1,24 @@
 /*************
 **
-** Project:      __PROJECT__
+** Project:      Colour-space-viewer
 ** File info:    $Id$
-** Author:       Copyright (C) 2010 Kuzma Shapran <Kuzma.Shapran@gmail.com>
+** Author:       Copyright (C) 2011 Kuzma Shapran <Kuzma.Shapran@gmail.com>
 ** License:      GPLv3
 **
-**  This file is part of __PROJECT__.
+**  This file is part of Colour-space-viewer.
 **
-**  __PROJECT__ is free software: you can redistribute it and/or modify
+**  Colour-space-viewer is free software: you can redistribute it and/or modify
 **  it under the terms of the GNU General Public License as published by
 **  the Free Software Foundation, either version 3 of the License, or
 **  (at your option) any later version.
 **
-**  __PROJECT__ is distributed in the hope that it will be useful,
+**  Colour-space-viewer is distributed in the hope that it will be useful,
 **  but WITHOUT ANY WARRANTY; without even the implied warranty of
 **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 **  GNU General Public License for more details.
 **
 **  You should have received a copy of the GNU General Public License
-**  along with __PROJECT__.  If not, see <http://www.gnu.org/licenses/>.
+**  along with Colour-space-viewer.  If not, see <http://www.gnu.org/licenses/>.
 **
 *************/
 
